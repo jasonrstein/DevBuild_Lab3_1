8f094795a99979b5381f5a0b4f94fbcac382d38b
Jason Stein <jasonstein@quickenloans.com>
2021-03-22 15:00:48 -0400

    First Commit

---
+++ DevBuild_Lab3_1/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace DevBuild_Lab3_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("This is Lab 3.1!");
+        }
+    }
+}
